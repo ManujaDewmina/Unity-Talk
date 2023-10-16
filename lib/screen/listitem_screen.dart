4318ba48a20d28d9cd20a55e3b787402703f2917
ManujaDewmina <manuja1bc@gmail.com>
2023-10-17 00:42:14 +0530

    version 2.0

--- lib/screen/listitem_screen.dart
+++ lib/screen/listitem_screen.dart
@@ -1,17 +1,34 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ethic_app/reusable_widgets/reusable_widget.dart';
 import '../Utils/color_utils.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ListItemScreen extends StatefulWidget {
-  const ListItemScreen(String itemTitle, {Key? key}) : super(key: key);
+  final String itemTitle;
+  final String itemId;
+  final String itemUser;
+  final String itemDescription;
+  final String itemImageUrl;
+
+  const ListItemScreen(this.itemTitle, this.itemId, this.itemUser,
+      this.itemDescription, this.itemImageUrl,
+      {Key? key})
+      : super(key: key);
 
   @override
-  State<ListItemScreen> createState() => _ListItemScreenState();
+  State<ListItemScreen> createState() => _ListItemScreenState(
+      itemTitle, itemId, itemUser, itemDescription, itemImageUrl);
 }
 
 class _ListItemScreenState extends State<ListItemScreen> {
+  final String itemTitle;
+  final String itemId;
+  final String itemUser;
+  final String itemDescription;
+  final String itemImageUrl;
+
+  _ListItemScreenState(this.itemTitle, this.itemId, this.itemUser,
+      this.itemDescription, this.itemImageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +58,45 @@ class _ListItemScreenState extends State<ListItemScreen> {
           padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
           child: Column(
             children: <Widget>[
-
+              Card(
+                elevation: 4,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                      'Topic: $itemTitle',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Author: $itemUser',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Description: $itemDescription',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Image.network(
+                        itemImageUrl,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
